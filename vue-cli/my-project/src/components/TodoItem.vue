f3898c9f584af9a1949c74defc878e5532b3851d
humorliang <1174749673@qq.com>
2018-05-27 17:29:39 +0800

    vue-cli todolist demo over

--- vue-cli/my-project/src/components/TodoItem.vue
+++ vue-cli/my-project/src/components/TodoItem.vue
@@ -1,9 +1,20 @@
 <template>
+<div>
+    <div @click="handleDelete">{{content}}</div>
+</div>
 </template>
 
 <script>
 export default {
-}
+    //通过props接受父组件的传值
+    props:['content','index'],
+    methods:{
+        handleDelete(){
+            //子组件通过自定事件向父组件传值 $emit('事件名','执行事件函数时接受的参数')
+            this.$emit('delete','index')
+        }
+    }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
